1315666142f62ef290c9ac5cc25c748ec1e643b2
Wang Yanjie <wangyanjie_pwd@163.com>
2016-02-29 20:59:53 +0800

    开放给前端的接口

--- ele/lottery/views.py
+++ ele/lottery/views.py
@@ -1,3 +1,38 @@
+# -*- coding:utf-8 -*-
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_number(request):
+    '''
+    用户获取一个抽奖号码
+    '''
+    pass
+
+
+def list_numbers(request):
+    '''
+    用户查看自己的所有抽奖号码
+    '''
+    pass
+
+
+def get_wining_number(request):
+    '''
+    获取中奖号码
+    '''
+    pass
+
+
+def get_wining_user(request):
+    '''
+    获取中奖用户
+    '''
+    pass
+
+
+def count_number(request):
+    '''
+    返回当前已抽取的抽奖号码的数量
+    '''
+    pass
